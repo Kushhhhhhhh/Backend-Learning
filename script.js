ee03f489e401bfffdc56fc4b310b2ea9c0f267ca
Kush <kush.73888@gmail.com>
2023-10-08 17:13:35 +0530

    last

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
-
 app.get('/', function(req,res) {
     res.send('hello world');
 })
-
-app.listen(2323);
+app.get('/kush', function(req,res) {
+    res.send('hello kush bhai ki haal chaal');
+})
+app.listen(3000);
